2524752f1464bc000e9c380aa8bfc9b3c0e0ee38
Adrian Palcic <adrian.palcic@gmail.com>
2026-01-25 17:45:20 +0100

    initial loading

--- my-app/app/components/Destinations.tsx
+++ my-app/app/components/Destinations.tsx
@@ -3,6 +3,7 @@
 import { destinacije } from "@/constants";
 import Image from "next/image";
 import { useState } from "react";
+import Preloader from "./Preloader";
 
 interface Destination {
   id: number;
@@ -36,103 +37,106 @@ const Destinations = () => {
   };
 
   return (
-    <main className="w-full h-screen relative overflow-hidden">
-      {/* BACKGROUND IMAGE - S FADE EFEKTOM */}
-      <div className="absolute inset-0 z-0">
-        <Image
-          key={currentDestination.backgroundImage} // Ključno: React re-renderira sliku pri promjeni
-          src={currentDestination.backgroundImage}
-          sizes="100vw"
-          alt="Adrion travel agency destinations"
-          fill
-          priority
-          className="object-cover object-center bg-image-fade transition-opacity duration-1000"
-        />
-      </div>
-
-      <div className="absolute inset-0 bg-black/50 z-10"></div>
-
-      {/* CONTENT */}
-      <div
-        id="content"
-        className="z-20 w-full h-full relative flex flex-col items-center"
-      >
-        <div className="my-6 mx-1">
-          <h1 className="font-pinyon  text-3xl lg:text-6xl text-white text-center">
-            Where our journeys take place
-          </h1>
+    <>
+      <main className="w-full h-screen relative overflow-hidden">
+        {/* BACKGROUND IMAGE - S FADE EFEKTOM */}
+        <div className="absolute inset-0 z-0">
+          <Image
+            key={currentDestination.backgroundImage} // Ključno: React re-renderira sliku pri promjeni
+            src={currentDestination.backgroundImage}
+            sizes="100vw"
+            alt="Adrion travel agency destinations"
+            fill
+            quality={75}
+            priority
+          />
         </div>
 
-        <div className="relative flex flex-col items-center justify-center flex-1 w-full">
-          <div className="relative group shadow-2xl">
-            <Image
-              src={currentDestination.frontImage}
-              width={400} // Rezervira prostor za veći ekran
-              height={500}
-              alt={currentDestination.name}
-              // Ispod 'sm' (mobitel) je w-[300px] h-[400px]
-              // Od 'sm' pa na gore (desktop) je w-[400px] h-[500px]
-              className="object-cover w-[300px] h-[400px] sm:w-[400px] sm:h-[500px] transition-all duration-500"
-            />
+        <div className="absolute inset-0 bg-black/50 z-10"></div>
 
-            <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/40 group hover:bg-black/70 transition-colors duration-300">
-              <h2 className="text-white text-center text-6xl lg:text-9xl font-cormorant font-medium italic tracking-wider drop-shadow-lg ">
-                {currentDestination.name}
-              </h2>
+        {/* CONTENT */}
+        <div
+          id="content"
+          className="z-20 w-full h-full relative flex flex-col items-center"
+        >
+          <div className="my-6 mx-1">
+            <h1 className="font-pinyon  text-3xl lg:text-6xl text-white text-center">
+              Where our journeys take place
+            </h1>
+          </div>
 
-              <p className="text-white font-ovo text-[18px] text-center opacity-80 mt-6 max-w-xs px-4">
-                {currentDestination.description}
-              </p>
+          <div className="relative flex flex-col items-center justify-center flex-1 w-full">
+            <div className="relative group shadow-2xl">
+              <Image
+                src={currentDestination.frontImage}
+                width={400} // Rezervira prostor za veći ekran
+                height={500}
+                alt={currentDestination.name}
+                // Ispod 'sm' (mobitel) je w-[300px] h-[400px]
+                // Od 'sm' pa na gore (desktop) je w-[400px] h-[500px]
+                className="object-cover w-[300px] h-[400px] sm:w-[400px] sm:h-[500px] transition-all duration-500"
+              />
 
-              <button className="group flex flex-col items-center gap-2 cursor-pointer mt-4 opacity-0 group-hover:opacity-100 transition-opacity duration-1000">
-                <span className="text-(--yellow) uppercase tracking-[0.3em] font-bold font-cormorant opacity-80 group-hover:opacity-100 transition-opacity duration-500 text-sm sm:text-[16px]">
-                  Explore Destination
-                </span>
-                <div className="w-24 h-px bg-(--yellow)/20 relative overflow-hidden">
-                  {/* Linija kreće izvan okvira i ulazi unutra na hover */}
-                  <div className="absolute inset-0 bg-(--yellow) -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-out" />
-                </div>
+              <div className="absolute inset-0 flex flex-col items-center justify-center bg-black/40 group hover:bg-black/70 transition-colors duration-300">
+                <h2 className="text-white text-center text-6xl lg:text-9xl font-cormorant font-medium italic tracking-wider drop-shadow-lg ">
+                  {currentDestination.name}
+                </h2>
+
+                <p className="text-white font-ovo text-[18px] text-center opacity-80 mt-6 max-w-xs px-4">
+                  {currentDestination.description}
+                </p>
+
+                <button className="group flex flex-col items-center gap-2 cursor-pointer mt-4 opacity-0 group-hover:opacity-100 transition-opacity duration-1000">
+                  <span className="text-(--yellow) uppercase tracking-[0.3em] font-bold font-cormorant opacity-80 group-hover:opacity-100 transition-opacity duration-500 text-sm sm:text-[16px]">
+                    Explore Destination
+                  </span>
+                  <div className="w-24 h-px bg-(--yellow)/20 relative overflow-hidden">
+                    {/* Linija kreće izvan okvira i ulazi unutra na hover */}
+                    <div className="absolute inset-0 bg-(--yellow) -translate-x-full group-hover:translate-x-0 transition-transform duration-500 ease-out" />
+                  </div>
+                </button>
+              </div>
+            </div>
+          </div>
+
+          {/* BOUTIQUE NAVIGATION */}
+          <div className="absolute bottom-12 right-12 z-40 flex items-center gap-8">
+            {/* Brojač */}
+            <div className="flex flex-col items-start">
+              <span className="text-white text-4xl font-light">
+                0
+                {destinacije.findIndex((d) => d.id === currentDestination.id) +
+                  1}
+              </span>
+              <div className="w-12 h-px bg-white/30 mt-1 relative">
+                <div
+                  className="absolute top-0 left-0 h-full bg-white transition-all duration-500"
+                  style={{
+                    width: `${((destinacije.findIndex((d) => d.id === currentDestination.id) + 1) / destinacije.length) * 100}%`,
+                  }}
+                />
+              </div>
+            </div>
+
+            {/* Gumbi */}
+            <div className="flex gap-4">
+              <button
+                onClick={handlePrev}
+                className="w-12 cursor-pointer h-12 border border-white/20 rounded-full flex items-center justify-center text-white hover:bg-(--green)  transition-colors duration-200"
+              >
+                <span className="text-xl">←</span>
+              </button>
+              <button
+                onClick={handleNext}
+                className="w-12 cursor-pointer h-12 border border-white/20 rounded-full flex items-center justify-center text-white hover:bg-(--green)  transition-colors duration-200"
+              >
+                <span className="text-xl">→</span>
               </button>
             </div>
           </div>
         </div>
-
-        {/* BOUTIQUE NAVIGATION */}
-        <div className="absolute bottom-12 right-12 z-40 flex items-center gap-8">
-          {/* Brojač */}
-          <div className="flex flex-col items-start">
-            <span className="text-white text-4xl font-light">
-              0
-              {destinacije.findIndex((d) => d.id === currentDestination.id) + 1}
-            </span>
-            <div className="w-12 h-px bg-white/30 mt-1 relative">
-              <div
-                className="absolute top-0 left-0 h-full bg-white transition-all duration-500"
-                style={{
-                  width: `${((destinacije.findIndex((d) => d.id === currentDestination.id) + 1) / destinacije.length) * 100}%`,
-                }}
-              />
-            </div>
-          </div>
-
-          {/* Gumbi */}
-          <div className="flex gap-4">
-            <button
-              onClick={handlePrev}
-              className="w-12 cursor-pointer h-12 border border-white/20 rounded-full flex items-center justify-center text-white hover:bg-(--green)  transition-colors duration-200"
-            >
-              <span className="text-xl">←</span>
-            </button>
-            <button
-              onClick={handleNext}
-              className="w-12 cursor-pointer h-12 border border-white/20 rounded-full flex items-center justify-center text-white hover:bg-(--green)  transition-colors duration-200"
-            >
-              <span className="text-xl">→</span>
-            </button>
-          </div>
-        </div>
-      </div>
-    </main>
+      </main>
+    </>
   );
 };
 
--- my-app/app/components/Hero.tsx
+++ my-app/app/components/Hero.tsx
@@ -1,42 +1,53 @@
+"use client";
+
 import Image from "next/image";
+import Preloader from "./Preloader";
+import { useState } from "react";
 
 const Hero = () => {
+  const [isFirstImageLoaded, setIsFirstImageLoaded] = useState(false);
+
   return (
-    <section className="relative w-full h-screen overflow-hidden bg-black">
-      <Image
-        src="/images/hero.webp"
-        alt="Boutique Agency Experience"
-        fill
-        priority
-        quality={100}
-        sizes="100vw"
-        className="object-cover object-center pointer-events-none hero-image-sharp"
-        // placeholder="blur" // Ako imaš generiran blurDataURL, ovo je vrhunski
-      />
+    <>
+      <Preloader isReady={isFirstImageLoaded} />
 
-      <div className="absolute inset-0 bg-black/50" />
+      <section className="relative w-full h-screen overflow-hidden bg-black">
+        <Image
+          src="/images/hero.webp"
+          alt="Boutique Agency Experience"
+          fill
+          priority
+          quality={100}
+          sizes="100vw"
+          className="object-cover object-center pointer-events-none hero-image-sharp"
+          onLoad={() => setIsFirstImageLoaded(true)}
+          // placeholder="blur" // Ako imaš generiran blurDataURL, ovo je vrhunski
+        />
 
-      {/* CONTENT */}
-      <div className="relative z-10 h-full flex flex-col justify-center items-center text-center px-6 mt">
-        <h1 className="main-title text-3xl sm:text-5xl md:text-[70px] text-white max-w-5xl leading-[1.1]">
-          Curated Croatian <br />
-          <span className="italic">Journeys</span>
-        </h1>
+        <div className="absolute inset-0 bg-black/50" />
 
-        <p className="main-subtitle text-white/90 mt-6 max-w-2xl text-xl md:text-3xl">
-          For those who appreciate quality, balance and authentic experiences
-        </p>
+        {/* CONTENT */}
+        <div className="relative z-10 h-full flex flex-col justify-center items-center text-center px-6 mt">
+          <h1 className="main-title text-3xl sm:text-5xl md:text-[70px] text-white max-w-5xl leading-[1.1]">
+            Curated Croatian <br />
+            <span className="italic">Journeys</span>
+          </h1>
 
-        <button className="group flex flex-col items-center gap-2 cursor-pointer mt-12">
-          <span className="text-white uppercase tracking-[0.3em] font-bold  font-cormorant text-sm sm:text-[16px]">
-            Start Your Adventure
-          </span>
-          <div className="w-24 h-px bg-white/20 relative overflow-hidden">
-            <div className="absolute inset-0 bg-white translate-x-full group-hover:translate-x-[0%] transition-transform duration-500" />
-          </div>
-        </button>
-      </div>
-    </section>
+          <p className="main-subtitle text-white/90 mt-6 max-w-2xl text-xl md:text-3xl">
+            For those who appreciate quality, balance and authentic experiences
+          </p>
+
+          <button className="group flex flex-col items-center gap-2 cursor-pointer mt-12">
+            <span className="text-white uppercase tracking-[0.3em] font-bold  font-cormorant text-sm sm:text-[16px]">
+              Start Your Adventure
+            </span>
+            <div className="w-24 h-px bg-white/20 relative overflow-hidden">
+              <div className="absolute inset-0 bg-white translate-x-full group-hover:translate-x-[0%] transition-transform duration-500" />
+            </div>
+          </button>
+        </div>
+      </section>
+    </>
   );
 };
 
